2f5dd97360dd655898cc0eb35be66f1d5cb48d4d
adildssp2 <adinour@free.fr>
2015-07-03 15:53:33 +0200

    Update feature_extractor.py

--- feature_extractor.py
+++ feature_extractor.py
@@ -28,12 +28,12 @@ class FeatureExtractor(object):
         
         #uncomment the line below in the submission
         #path = os.path.dirname(__file__)
-        data_weather = pd.read_csv("data_weather.csv")
+        #data_weather = pd.read_csv("data_weather.csv")
         
-        X_weather = data_weather[['Date', 'AirPort', 'Max TemperatureC']]
-        X_weather = X_weather.rename(columns={'Date': 'DateOfDeparture', 'AirPort': 'Arrival'})
+        #X_weather = data_weather[['Date', 'AirPort', 'Max TemperatureC']]
+        #X_weather = X_weather.rename(columns={'Date': 'DateOfDeparture', 'AirPort': 'Arrival'})
         X_encoded = X_encoded.set_index(['DateOfDeparture', 'Arrival'])
-        X_weather = X_weather.set_index(['DateOfDeparture', 'Arrival'])
+        #X_weather = X_weather.set_index(['DateOfDeparture', 'Arrival'])
         X_encoded = X_encoded.join(X_weather).reset_index()
         
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['Departure'], prefix='d'))
