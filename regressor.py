72f4a460d111d8e81ae4f26e03f7a0352ab3b835
adildssp2 <adinour@free.fr>
2015-07-04 10:42:48 +0200

    Update regressor.py

--- regressor.py
+++ regressor.py
@@ -1,13 +1,12 @@
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import AdaBoostRegressor
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.base import BaseEstimator
-
+ 
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.clf = AdaBoostRegressor(RandomForestRegressor(n_estimators=50, max_depth=50, max_features=25), n_estimators=20)
-
+        self.clf = GradientBoostingRegressor( n_estimators = 1000 , max_depth = 5 , max_features = 10)
+        
     def fit(self, X, y):
         self.clf.fit(X, y)
-
+ 
     def predict(self, X):
         return self.clf.predict(X)
