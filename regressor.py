65e79525950d0ae91b38467a9a0e8bf042f6e1a8
adildssp2 <adinour@free.fr>
2015-07-04 11:01:11 +0200

    Update regressor.py

--- regressor.py
+++ regressor.py
@@ -3,7 +3,7 @@ from sklearn.base import BaseEstimator
  
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.clf = GradientBoostingRegressor( n_estimators = 1000 , max_depth = 5 , max_features = 10)
+        self.clf = GradientBoostingRegressor( n_estimators = 1500 , max_depth = 7 , max_features = 15)
         
     def fit(self, X, y):
         self.clf.fit(X, y)
